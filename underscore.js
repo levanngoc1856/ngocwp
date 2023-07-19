5c9e6d5310bc9ffe0ba99dac4b018e190d88f414
levanngoc1856 <136575378+levanngoc1856@users.noreply.github.com>
2023-07-19 11:24:29 +0700

    Update underscore.js

--- underscore.js
+++ underscore.js
@@ -1,7 +1,8 @@
-//     Underscore.js 1.8.2
+//     Underscore.js 1.8.3
 //     http://underscorejs.org
 //     (c) 2009-2015 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
 //     Underscore may be freely distributed under the MIT license.
+// update new version
 
 (function() {
 
